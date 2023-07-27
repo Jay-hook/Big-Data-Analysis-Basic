433361c96abf56a6e1555f4a8a4651332b5fb1a0
bsmoon-86 <ekflafg@naver.com>
2023-07-27 12:42:53 +0900

    update

---
+++ R/R기초2.R
@@ -0,0 +1,134 @@
+# 데이터프레임 
+name = c('test', 'test2', 'test3', 'test4')
+grade = c(1, 3, 2, 1)
+
+student = data.frame(name, grade)
+print(student)
+
+score = c(60, 70, 80)
+
+# 데이터프레임 생성시에는 같은 길이의 백터를 결합해야된다. 
+# 길이가 다를 시에는 에러가 발생
+data.frame(name, score)
+
+
+# 행렬 생성 
+midturm = c(70, 80, 60, 90)
+final = c(60, 90, 80, 90)
+score = cbind(midturm, final) 
+print(score)
+
+# 백터의 합
+total_score = midturm + final 
+print(total_score)
+
+# 데이터프레임 + 행렬 + 백터 -> 새로운 데이터프레임을 생성
+print(data.frame(student, score))
+print(data.frame(student, total_score))
+students = data.frame(student, score, total_score)
+print(students)
+
+# 새로운 컬럼을 추가 
+gender = c("F", "M", "M", "F")
+# data.frame()을 이용하여 새로운 데이터프레임 생성
+print(data.frame(students, gender))
+# cbind()을 이용하여 컬럼을 추가한다. 
+print(cbind(students, gender))
+
+print(students)
+
+# 데이터프레임의 필터 기능 
+# 하나의 열을 선택
+print(students$name)
+print(students[["name"]])
+print(students[[1]])
+
+# 인덱스를 필터
+# 데이터프레임[행의 조건, 열의 조건]
+print(students[1,])
+print(students[c(2, 4), ])
+print(students[1:3, ])
+
+# 조건식 하나 생성
+students[['grade']] >= 2
+
+students[students[['grade']] >= 2, ]
+
+# 중간의 성적이 70점 이상이고, 
+# 기말의 성적이 90점 이상인 학생의 정보 출력
+students$midturm >= 70 & students$final >= 90 -> flag
+
+students[flag, ]
+
+# 새로운 파생변수를 생성 (컬럼 추가)
+students$gender = gender
+print(students)
+
+# 성별이 "M"인 학생의 중간 성적과 기말 성적을 출력
+students[students$gender == 'M', c('midturm', 'final')]
+
+# 데이터프레임에서 새로운 열을 추가 
+
+new_student = data.frame(name = 'test5', 
+                         grade = 2,  
+                         gender = 'F', 
+                         midturm = 90, 
+                         final = 70, 
+                         total_score = 160)
+print(new_student)
+# rbind()를 이용하여 새로운 데이터프레임에 행을 추가 
+rbind(students, new_student)
+
+# 함수 생성 
+# 매개변수 2개 생성(df, c)
+# df에는 데이터프레임, c에는 컬럼의 위치
+# 데이터프레임의 특정한 열을 출력
+# c의 기본값은 1로 지정
+func_1 = function(df, c=1){
+  result = df[[c]]
+  return (result)
+}
+func_1(students, 6)
+func_1(students)
+
+# 데이터프레임의 정렬을 변경 
+# order(데이터프레임$컬럼) -> 
+  # 해당하는 컬럼의 데이터를 오름차순 정렬로 행의 값들은 정렬
+order(students$grade, decreasing = FALSE)
+students[order(students$grade), ]
+
+# 내림차순 정렬로 데이터의 순서를 변경
+# order(데이터프레임$컬럼, decreasing=TRUE)
+order(students$grade, decreasing = TRUE)
+students[order(students$grade, decreasing = TRUE), ]
+
+# 경로 설정 
+# 절대 경로 
+  # 절대적인 주소
+  # 환경이 변하더라도 위치는 변하지 않는다. 
+  # ex) c:/users/admin/documents, url(www.google.com, www.naver.com)
+# 상대 경로
+  # 상대적인 주소
+  # 환경이 변하면 유동성이 있게 위치가 변화한다. 
+  # ./ : 현재 파일의 위치
+  # ../ : 현재 위치에서 상위로 이동
+  # 폴더명/ : 해당하는 하위 폴더로 이동
+
+# 외부의 있는 데이터파일 로드 
+# read.csv(파일의 위치 지정)
+read.csv("./csv_exam.csv")
+
+# R폴더에서 -> 상위 폴더로 이동 -> csv 하위폴더 이동 -> csv_exam.csv
+# 절대 경로
+read.csv("C:/Users/moons/Documents/GitHub/ubion-8/csv/csv_exam.csv")
+# 상대 경로
+read.csv("../csv/csv_exam.csv")
+
+
+
+
+
+
+
+
+
